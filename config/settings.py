a64b22c3c0a53e44516e6cea3f42fac88b720c45
heba <hysaleh@memphis.edu>
2026-04-25 14:56:53 -0500

    debug true temp

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1", "roadmap-app-wnqd.onrender.com"]
 
