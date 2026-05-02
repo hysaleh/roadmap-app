6849d3787b790d2b9a14ccb976abb17ff431cc9d
heba <hysaleh@memphis.edu>
2026-05-01 20:00:54 -0500

    added brackets to sk

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / ".env")
-GEMINI_API_KEY = os.environ("GEMINI_API_KEY")
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
